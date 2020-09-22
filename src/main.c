69895e6d9a0c4173ea34de6cfdca16181a4c4e09
Nicolas BAUW <nbauw@hotmail.com>
2020-09-22 13:30:58 +0200

    Trailing newline

--- src/main.c
+++ src/main.c
@@ -2,10 +2,17 @@
 
 int bin2source(char inputfile[], char outputfile[], char tablename[]);
 
+const char *HELP = "Creates a C source array from a file.\n\n\
+USAGE:\n\
+    bin2source inputfile outputfile tablename\n\
+\n\
+EXAMPLE:\n\
+    bin2source test test.c TESTARRAY\n";
+
 int main( int argc, char *argv[] )  {
 
    if( argc == 4 ) {
       bin2source(argv[1], argv[2], argv[3]);
    }
-   else printf("Invalid number of arguments.\n");
+   else printf("%s", HELP);
 }
