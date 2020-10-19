7664b2f1c828ca98cd2fcf3e3274c05790dff0fc
Nicolas BAUW <nbauw@hotmail.com>
2020-10-19 17:41:12 +0200

    Amiga mode

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int bin2source(char *inputfile, char *outputfile, char *tablename, int amiga);
 
@@ -10,17 +11,22 @@ EXAMPLE:\n\
     bin2source test test.c TESTARRAY\n";
 
 int main( int argc, char *argv[] )  {
+    // Amiga mode ?
     int amiga;
-    if( argc == 4 || 5) {
-    if (argv[4] == "--amiga") { amiga = 1; } else { amiga = 0; }
-    switch (bin2source(argv[1], argv[2], argv[3], amiga)) {
-        case 0: return 0;
+    if( argc == 4 || argc == 5) {
+        if (argv[4]) {
+            if (strncmp(argv[4], "--amiga", 7) == 0) amiga = 1;
+        }
+        else amiga = 0;
+        
+        switch (bin2source(argv[1], argv[2], argv[3], amiga)) {
+            case 0: return 0;
 
-        case 1: printf("Can't open input file\n");
-        return 1;
+            case 1: printf("Can't open input file\n");
+            return 1;
 
-        case 2: printf("Can't open output file\n");
-        return 2;
+            case 2: printf("Can't open output file\n");
+            return 2;
         }
     }
     else printf("%s", HELP);
